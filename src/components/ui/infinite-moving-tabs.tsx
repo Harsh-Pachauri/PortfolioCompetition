47e2207212841cf95e11d9ec081c1d333755b97e
Harsh_Pachauri <harshpachauri205@gmail.com>
2024-12-06 01:14:36 +0530

    Add ui component for moving tabs

---
+++ src/components/ui/infinite-moving-tabs.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { cn } from "@/lib/utils/cn";
+import React, { useEffect, useState } from "react";
+
+export const InfiniteMovingTabs = ({
+  items,
+  direction = "left",
+  speed = "fast",
+  pauseOnHover = true,
+  className,
+}: {
+  items: { skill: string }[]; // Updated to only include a `skill` property
+  direction?: "left" | "right";
+  speed?: "fast" | "normal" | "slow";
+  pauseOnHover?: boolean;
+  className?: string;
+}) => {
+  const containerRef = React.useRef<HTMLDivElement>(null);
+  const scrollerRef = React.useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    addAnimation();
+  }, []);
+
+  const [start, setStart] = useState(false);
+
+  function addAnimation() {
+    if (containerRef.current && scrollerRef.current) {
+      const scrollerContent = Array.from(scrollerRef.current.children);
+
+      scrollerContent.forEach((item) => {
+        const duplicatedItem = item.cloneNode(true);
+        if (scrollerRef.current) {
+          scrollerRef.current.appendChild(duplicatedItem);
+        }
+      });
+
+      getDirection();
+      getSpeed();
+      setStart(true);
+    }
+  }
+
+  const getDirection = () => {
+    if (containerRef.current) {
+      containerRef.current.style.setProperty(
+        "--animation-direction",
+        direction === "left" ? "forwards" : "reverse"
+      );
+    }
+  };
+
+  const getSpeed = () => {
+    if (containerRef.current) {
+      const duration =
+        speed === "fast" ? "10s" : speed === "normal" ? "40s" : "80s";
+      containerRef.current.style.setProperty("--animation-duration", duration);
+    }
+  };
+
+  return (
+    <div
+      ref={containerRef}
+      className={cn(
+        "scroller relative z-20 max-w-full overflow-hidden [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
+        className
+      )}
+    >
+      <ul
+        ref={scrollerRef}
+        className={cn(
+          "flex min-w-full shrink-0 gap-4 py-4 flex-nowrap",
+          start && "animate-scroll",
+          pauseOnHover && "hover:[animation-play-state:paused]"
+        )}
+      >
+        {items.map((item, index) => (
+          <li
+            key={index}
+            className="w-[150px] md:w-[200px] max-w-full relative rounded-lg border border-gray-700 bg-gradient-to-b from-gray-800 to-gray-900 px-4 py-3 flex-shrink-0 text-center"
+          >
+            <span className="text-sm font-semibold text-gray-100">
+              {item.skill}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
